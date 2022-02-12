b016a582f24ae2b61843b107d635c7eb136d1fe7
Renato Sormani <ilgiardino@hotmail.co.uk>
2022-02-12 10:08:28 +0000

    Defined functions to JavaScript

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+//Create Calculator class - put a constructor that takes all the inputs and fundtions for the calculator. Takes previous and current operand elements.
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+        this.clear()
+      }
+
+      //defininig functions
+
+      clear () {
+
+      }
+
+      delete () {
+
+      }
+
+      appendNumber (number) {
+
+      }
+
+      compute () {
+
+      }
+
+updateDisplay() {
+
+}
+
+}
+
 //Our constant variables
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
