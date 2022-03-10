e809d6dceabfa759949319f3f6061a92be9e922f
Renato Sormani <ilgiardino@hotmail.co.uk>
2022-03-10 10:22:37 +0000

    Class names and styling changed for BEM

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
-//Create Calculator class - put a constructor that takes all the inputs and fundtions for the calculator. Takes previous and current operand elements.
+//SELECTORS
+const numberButtons = document.querySelectorAll('[data__number]')
+const operationButtons = document.querySelectorAll('[data__operation]')
+const equalsButton = document.querySelector('[data__equals]')
+const deleteButton = document.querySelector('[data__delete]')
+const allClearButton = document.querySelector('[data__all__clear]')
+const previousOperandTextElement = document.querySelector('[data__previous__operand]')
+const currentOperandTextElement = document.querySelector('[data__current__operand]')
+const firstLastSquares = document.querySelectorAll(".span__two")
+
+//Create Calculator class - put a constructor that takes all the inputs and functions for the calculator. Takes previous and current operand elements.
 class Calculator {
     constructor(previousOperandTextElement, currentOperandTextElement) {
       this.previousOperandTextElement = previousOperandTextElement
       this.currentOperandTextElement = currentOperandTextElement
       this.clear()
     }
-    
-    //defining functions
-
+  
+//DEFINING FUNCTIONS
     // In the clear function, need to remove all values
     // ie.currentOperand, previousOperand and operation, defaulting first two to empty strings and Op to undefined
     clear() {
-      this.currentOperand = ''
+      this.currentOperand = '' 
       this.previousOperand = ''
       this.operation = undefined
     }
@@ -21,15 +30,15 @@ class Calculator {
       this.currentOperand = this.currentOperand.toString().slice(0, -1)
     }
   
-//convert currentOperand and number to Strings so it's easy to append numbers instead of "adding" numbers
 // Only allow one decimal point to be added or if it includes a decimal point, simply return
+//convert currentOperand and number to Strings so it's easy to append numbers instead of "adding" numbers
     appendNumber(number) {
       if (number === '.' && this.currentOperand.includes('.')) return
       this.currentOperand = this.currentOperand.toString() + number.toString()
     }
   
 //Set the operation. Recycle the current value to the previous operand and type in a new value
-// If currentOperand is equal to an empty string set to return. If previous Operand doesn't eayal empty string, then compute
+// If currentOperand is equal to an empty string set to return. If previous Operand doesn't equal empty string, then compute
     chooseOperation(operation) {
       if (this.currentOperand === '') return
       if (this.previousOperand !== '') {
@@ -41,7 +50,7 @@ class Calculator {
     }
   
 // Set the variables computation, prev and current (converting strings to numbers). If the user presses = with no number, we want a return that cancels the function
-// Add if statements using switch for the operations. so set computation variable to prev and current with desired operation
+// Add 'if' statements using switch statement for the operations. so set computation variable to prev and current with desired operation
 // add else statement defined as default - so if other values aren't executed set return for nothing returned
     compute() {
       let computation
@@ -80,7 +89,7 @@ class Calculator {
       const decimalDigits = stringNumber.split('.')[1]
 
        // If integers isn't a number then display to empty string. But if a value is entered, use Locale String(english) and maximumfractiondigits ensures no more decimals after the first decimal.
-       //If theere are decimal digits concatanate integerDisplay and decimalDigits
+       //If there are decimal digits concatanate integerDisplay and decimalDigits
       let integerDisplay
       if (isNaN(integerDigits)) {
         integerDisplay = ''
@@ -108,19 +117,10 @@ class Calculator {
     }
   }
   
-  //Our constant variables
-  const numberButtons = document.querySelectorAll('[data-number]')
-  const operationButtons = document.querySelectorAll('[data-operation]')
-  const equalsButton = document.querySelector('[data-equals]')
-  const deleteButton = document.querySelector('[data-delete]')
-  const allClearButton = document.querySelector('[data-all-clear]')
-  const previousOperandTextElement = document.querySelector('[data-previous-operand]')
-  const currentOperandTextElement = document.querySelector('[data-current-operand]')
-  const firstLastSquares = document.querySelectorAll(".span-two")
-
   //create a calculator and pass everything from the constructor into it (ie the previousOperand and currentOperand text elements)
   const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
   
+  //NUMBER BUTTONS
   //select numberButtons and add forEach and event listeners to make buttons add number of whatever is in the innerText of the buttons and relay to display.
   numberButtons.forEach(button => {
     button.addEventListener('click', () => {
@@ -129,6 +129,7 @@ class Calculator {
     })
   })
   
+  //OPERATION BUTTONS
   //select operationButtons and add forEach and event listeners to make buttons add operation of whatever is in the innerText of the buttons and relay to display.
   operationButtons.forEach(button => {
     button.addEventListener('click', () => {
@@ -149,7 +150,7 @@ class Calculator {
     calculator.updateDisplay()
   })
   
-//delete function calls the clear function and update display
+//delete function calls the delete function and update display
   deleteButton.addEventListener('click', button => {
     calculator.delete()
     calculator.updateDisplay()
